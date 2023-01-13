f9e112fda07f8a6e3dedbfdeab6fdc2098a701d5
Omer Oreg <sarhatabaot1@gmail.com>
2023-01-13 18:06:06 +0200

    Update NoFlyListener.java

--- src/main/java/me/clip/noflyzone/NoFlyListener.java
+++ src/main/java/me/clip/noflyzone/NoFlyListener.java
@@ -1,55 +1,64 @@
 package me.clip.noflyzone;
 
+import com.github.sarhatabaot.kraken.core.chat.ChatUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class NoFlyListener implements Listener {
-	
-	NoFlyZone plugin;
-	
-	public NoFlyListener(NoFlyZone i) {
-		plugin = i;
-		Bukkit.getPluginManager().registerEvents(this, i);
-	}
-	
-	@EventHandler
-	public void onMove(PlayerMoveEvent e) {
-		
-		if (NoFlyZone.regions == null || NoFlyZone.regions.isEmpty()) {
-			return;
-		}
-		
-		if ((e.getFrom().getBlockX() != e.getTo().getBlockX()) || 
-		    (e.getFrom().getBlockY() != e.getTo().getBlockY()) || 
-		    (e.getFrom().getBlockZ() != e.getTo().getBlockZ())) {
-			
-			if (!e.getPlayer().isFlying()) {
-				return;
-			}
-			
-			if (NoFlyZone.getRegion(e.getTo()) == null) {
-				return;
-			}
-			
-			
-			if (NoFlyZone.regions.contains(NoFlyZone.getRegion(e.getTo()))) {
-				
-				if (e.getPlayer().hasPermission("noflyzone.bypass")) {
-					return;
-				}
-				
-				e.getPlayer().setFlying(false);
-				
-				if (NoFlyZone.message != null && !NoFlyZone.message.isEmpty()) {
-					plugin.sms(e.getPlayer(), NoFlyZone.message);
-				}
-				
-			}
-			
-		}
-		
-	}
-
+    
+    private final NoFlyZone plugin;
+    
+    public NoFlyListener(final NoFlyZone plugin) {
+        this.plugin = plugin;
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+    
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        
+        if (NoFlyZone.regions == null || NoFlyZone.regions.isEmpty()) {
+            return;
+        }
+        if (!(isSameXYZ(event.getFrom(), event.getTo()))) {
+            return;
+        }
+        
+        
+        if (!event.getPlayer().isFlying()) {
+            return;
+        }
+        
+        if (plugin.getRegion(event.getTo()) == null) {
+            return;
+        }
+        
+        
+        if (NoFlyZone.regions.contains(plugin.getRegion(event.getTo()))) {
+            
+            if (event.getPlayer().hasPermission("noflyzone.bypass")) {
+                return;
+            }
+            
+            event.getPlayer().setFlying(false);
+            
+            if (NoFlyZone.message != null && !NoFlyZone.message.isEmpty()) {
+                ChatUtil.sendMessage(event.getPlayer(), NoFlyZone.message);
+            }
+            
+        }
+        
+        
+    }
+    
+    private boolean isSameXYZ(final @NotNull Location getFrom, final @NotNull Location getTo) {
+        return getFrom.getBlockX() != getTo.getBlockX() ||
+            getFrom.getBlockY() != getTo.getBlockY() ||
+            getFrom.getBlockZ() != getTo.getBlockZ();
+    }
+    
+    
 }
